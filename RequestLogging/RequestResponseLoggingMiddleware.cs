37f2a91d868b7cf682cbea9667a7b1fad9a4d53b
justinjohnwilliams <justinjohnwilliams@gmail.com>
2017-03-14 20:50:33 -0500

    make it so

---
+++ RequestLogging/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestLogging
+{
+    public class RequestResponseLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public RequestResponseLoggingMiddleware(RequestDelegate next,
+                                                ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory
+                      .CreateLogger<RequestResponseLoggingMiddleware>();
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation(await FormatRequest(context.Request));
+
+            var originalBodyStream = context.Response.Body;
+
+            using (var responseBody = new MemoryStream())
+            {
+                context.Response.Body = responseBody;
+
+                await _next(context);
+
+                _logger.LogInformation(await FormatResponse(context.Response));
+                await responseBody.CopyToAsync(originalBodyStream);
+            }
+        }
+
+        private async Task<string> FormatRequest(HttpRequest request)
+        {
+            var body = request.Body;
+            request.EnableRewind();
+
+            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+            var bodyAsText = Encoding.UTF8.GetString(buffer);
+            request.Body = body;
+
+            return $"{request.Scheme} {request.Host}{request.Path} {request.QueryString} {bodyAsText}";
+        }
+
+        private async Task<string> FormatResponse(HttpResponse response)
+        {
+            response.Body.Seek(0, SeekOrigin.Begin);
+            var text = await new StreamReader(response.Body).ReadToEndAsync();
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            return $"Response {text}";
+        }
+    }
+
+    public static class RequestResponseLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestResponseLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestResponseLoggingMiddleware>();
+        }
+    }
+
+}
